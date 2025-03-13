945253fbf8340898e4f63c4719e3629f089d64e1
vmfreixas <vmfreixas@gmail.com>
2025-03-12 22:03:07 -0700

    Function 'get_civecs_from_data' to read civecs from the file  produced by Fortran after reading the binaries from NWChem.

---
+++ get_civecs_from_data.py
@@ -0,0 +1,35 @@
+#
+#   This function reads a '.data' file a returns the CI vector as a
+# Nocc x Nvir matrix for a given root. The '.data' file is generated
+# by a Fortran script that reads the binary '.civecs' files generated
+# by NWChem.
+#
+
+import numpy as np
+
+def get_civecs_from_data(fileName, root):
+    matrix = []
+    with open(fileName, 'r') as dataFile:
+        ciBlock = False
+        read = False
+        i = 0
+        for line in dataFile:
+            if read:
+                i += 1
+                row = [float(x) for x in line.split()]
+                matrix.append(row)
+                if i == Nocc:
+                    return matrix
+            if 'Root' in line and int(line.split()[1]) == root:
+                ciBlock = True
+            if ciBlock and 'Nocc' in line:
+                Nocc = int(line.split()[1])
+            if ciBlock and 'Nvir' in line:
+                Nvir = int(line.split()[1])
+                read = True 
+
+fileName = 'civecs.data'
+root = 1
+civec = get_civecs_from_data(fileName, 19)
+np.savetxt('civec_1.txt', civec, fmt = '%.8f')
+
