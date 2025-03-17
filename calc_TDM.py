f370a2a992e6a34801b7c3abb249ae27c662416e
vmfreixas <vmfreixas@gmail.com>
2025-03-16 21:11:06 -0700

    Function 'calc_TDM' extended for calculating TDM between excited  states when a second CI vector is provided.

--- calc_TDM.py
+++ calc_TDM.py
@@ -4,8 +4,15 @@
 
 import numpy as np
 
-def calc_TDM(mo, ci):
-    (Nocc, Nvir) = ci.shape
-    tdmMO = np.zeros((Nocc + Nvir, Nocc + Nvir), dtype = float)
-    tdmMO[:Nocc, Nocc:] = ci
-    return mo @ tdmMO @ mo.T
+def calc_TDM(mo, ci1, ci2 = ''):
+    if ci2 != '':
+        (Nocc, Nvir) = ci1.shape
+        tdmMO = np.zeros((Nocc + Nvir, Nocc + Nvir), dtype = float)
+        tdmMO[:Nocc, Nocc:] = ci1
+        return mo @ tdmMO @ mo.T
+    else:
+        (Nocc, Nvir) = ci1.shape
+        tdmMO = np.zeros((Nocc + Nvir, Nocc + Nvir), dtype = float)
+        tdmMO[:Nocc, :Nocc] = -1.0 * ci1 @ ci2.T
+        tdmMO[Nocc:, Nocc:] = ci1.T @ ci2
+        return mo @ tdmMO @ mo.T
