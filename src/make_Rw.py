633e69c8aaf4123d2600646ce6053bd30199441e
vmfreixas <vmfreixas@gmail.com>
2025-10-02 16:27:58 -0700

    Correction included for the indexing inconsistency between NWChem and Multiwfn!

--- src/make_Rw.py
+++ src/make_Rw.py
@@ -1,6 +1,11 @@
 import numpy as np
+from src.make_NWChem_Multiwfn_AO_index_permutation_matrix import make_NWChem_Multiwfn_AO_index_permutation_matrix
 
-def make_Rw(elecX, elecY, elecZ, magX, magY, magZ):
+def make_Rw(elecX, elecY, elecZ, magX, magY, magZ, moldenTemplate):
     rr = np.outer(elecX, magX) + np.outer(elecY, magY) + np.outer(elecZ, magZ)
     rrSym = (rr + rr.T) / 2
-    return rrSym.sum(axis = 0)
+    #Correcting the AO indices to ensure the index compability between NWChem and Multiwfn
+    dim = 2
+    permMatrix = make_NWChem_Multiwfn_AO_index_permutation_matrix(moldenTemplate, dim)
+    rrSymCorrected = permMatrix.T @ rrSym @ permMatrix
+    return rrSymCorrected.sum(axis = 0)
