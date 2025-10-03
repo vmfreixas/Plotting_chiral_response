5265d129352205bfa42bc5579897cae1d38cb91a
vmfreixas <vmfreixas@gmail.com>
2025-10-03 09:18:24 -0700

    Inverse indexing correction function added
    - Function name generalized to distinguish cases
    - Function passed to src
    - Working example updated!

--- src/make_NWChem_Multiwfn_AO_index_permutation_matrix.py
+++ src/make_NWChem_Multiwfn_AO_index_permutation_matrix.py
@@ -10,7 +10,7 @@ def custom_sign(x):
     s[s == 0] = 1
     return s
 
-def make_NWChem_Multiwfn_AO_index_permutation_matrix(moldenFile, dim = 1):
+def make_Multiwfn_as_NWChem_AO_index_permutation_matrix(moldenFile, dim = 1):
     ao_index = []
     with open(moldenFile, 'r') as moldenF:
         count = -1
@@ -24,13 +24,49 @@ def make_NWChem_Multiwfn_AO_index_permutation_matrix(moldenFile, dim = 1):
                 elif '  p  ' in line:
                     for _ in range(3):
                         count += 1
-                        ao_index.append(count)
+                        ao_index.append(-count)
                 elif '  d  ' in line:
                         ao_index.append(count + 1 + 2)
                         ao_index.append(count + 1 + 3)
                         ao_index.append(count + 1 + 1)
                         ao_index.append(-(count + 1 + 4)) # Sign correction here!
                         ao_index.append(count + 1)
+                        count += 5
+                elif '  f  ' in line:
+                    raise ValueError("f functions not implemented yet")
+                    return(None)
+    if dim == 1:
+        return(ao_index)
+    elif dim == 2:
+        signs = custom_sign(ao_index)
+        eyeSign = np.eye(len(ao_index)) * signs[:, np.newaxis]
+        perm_matrix = eyeSign[np.abs(ao_index)]
+        return(perm_matrix)
+    else:
+        raise ValueError("dim has to be 1 or 2")
+        return(None)
+    
+def make_NWChem_as_Multiwfn_AO_index_permutation_matrix(moldenFile, dim = 1):
+    ao_index = []
+    with open(moldenFile, 'r') as moldenF:
+        count = -1
+        for line in moldenF:
+            if line.split() == []:
+                continue
+            else:
+                if '  s  ' in line:
+                    count += 1
+                    ao_index.append(count)
+                elif '  p  ' in line:
+                    for _ in range(3):
+                        count += 1
+                        ao_index.append(-count)
+                elif '  d  ' in line:
+                        ao_index.append(count + 1 + 4)
+                        ao_index.append(-(count + 1 + 2))
+                        ao_index.append(count + 1)
+                        ao_index.append(count + 1 + 1) 
+                        ao_index.append(count + 1 + 3)
                         count += 5
                 elif '  f  ' in line:
                     raise ValueError("f functions not implemented yet")
--- src/make_Rw.py
+++ src/make_Rw.py
@@ -1,11 +1,11 @@
 import numpy as np
-from src.make_NWChem_Multiwfn_AO_index_permutation_matrix import make_NWChem_Multiwfn_AO_index_permutation_matrix
+from src.make_NWChem_Multiwfn_AO_index_permutation_matrix import make_NWChem_as_Multiwfn_AO_index_permutation_matrix
 
 def make_Rw(elecX, elecY, elecZ, magX, magY, magZ, moldenTemplate):
     rr = np.outer(elecX, magX) + np.outer(elecY, magY) + np.outer(elecZ, magZ)
     rrSym = (rr + rr.T) / 2
     #Correcting the AO indices to ensure the index compability between NWChem and Multiwfn
     dim = 2
-    permMatrix = make_NWChem_Multiwfn_AO_index_permutation_matrix(moldenTemplate, dim)
+    permMatrix = make_NWChem_as_Multiwfn_AO_index_permutation_matrix(moldenTemplate, dim)
     rrSymCorrected = permMatrix.T @ rrSym @ permMatrix
     return rrSymCorrected.sum(axis = 0)
